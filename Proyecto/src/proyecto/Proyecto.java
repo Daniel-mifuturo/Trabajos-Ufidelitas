748858b53ea94bc130276892d3009537c3a150cb
Daniel-mifuturo <ramiel999@gmail.com>
2025-07-13 22:13:29 -0600

    Menu actualizado

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -4,129 +4,163 @@
  */
 package proyecto;
 import javax.swing.JOptionPane;
-import static proyecto.FechayHora.mostrarHorafecha;
 
 /**
  *
  * @author Grupo 7
  */
-public class Proyecto {
+public class Proyecto { 
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        mostrarMenuPrincipal();
+        int opcionPrincipal;
+        do {
+            String seleccion = JOptionPane.showInputDialog(
+                " MENÚ PRINCIPAL\n" +
+                "1. Administrar Libros\n" +
+                "2. Préstamos y Devoluciones\n" +
+                "3. Consultar\n" +
+                "4. Ver Fecha y Hora\n" +
+                "5. Salir\n\n" +
+                "Seleccione una opción:"
+            );
+
+            if (seleccion == null) break; // Usuario cerró el diálogo
+
+            opcionPrincipal = Integer.parseInt(seleccion);
+
+            switch (opcionPrincipal) {
+                case 1:
+                    administrarLibros();
+                    break;
+                case 2:
+                    prestamosDevoluciones();
+                    break;
+                case 3:
+                    consultarLibros();
+                    break;
+                case 4:
+                    FechayHora.mostrarHorafecha();
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Gracias por usar el sistema. ¡Hasta luego!");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+
+        } while (opcionPrincipal != 5);
     }
 
-    public static void mostrarMenuPrincipal() {
-        String seleccion = JOptionPane.showInputDialog(
-            " MENÚ PRINCIPAL\n" +
-            "1. Administrar Libros\n" +
-            "2. Préstamos y Devoluciones\n" +
-            "3. Consultar\n" +
-            "4. Ver Fecha y Hora\n" +
-            "5. Salir\n\n" +
-            "Seleccione una opción:"
-        );
+    public static void administrarLibros() {
+        int opcion;
 
-        if (seleccion.equals("1")) {
-            mostrarMenuAdministrarLibros();
-        } else if (seleccion.equals("2")) {
-            mostrarMenuPrestamosDevoluciones();
-        } else if (seleccion.equals("3")) {
-            mostrarMenuConsultar();
-        } else if (seleccion.equals("4")) {
+        do {
+            String seleccion = JOptionPane.showInputDialog(
+                " ADMINISTRAR LIBROS\n" +
+                "1. Agregar Libro\n" +
+                "2. Modificar Libro\n" +
+                "3. Eliminar Libro\n" +
+                "4. Volver al Menú Principal\n\n" +
+                "Seleccione una opción:"
+            );
 
-            mostrarHorafecha();
-            mostrarMenuPrincipal();
-        } else if (seleccion.equals("5")) {
-            JOptionPane.showMessageDialog(null, "Gracias por usar el sistema. ¡Hasta luego!");
-        } else {
-            JOptionPane.showMessageDialog(null, "Opción inválida.");
-            mostrarMenuPrincipal();
-        }
+            if (seleccion == null) break;
+
+            opcion = Integer.parseInt(seleccion);
+
+            switch (opcion) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Agregar Libro");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Modificar Libro");
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Eliminar Libro");
+                    break;
+                case 4:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+
+        } while (opcion != 4);
     }
 
-    public static void mostrarMenuAdministrarLibros() {
-        String seleccion = JOptionPane.showInputDialog(
-            " ADMINISTRAR LIBROS\n" +
-            "1. Agregar Libro\n" +
-            "2. Modificar Libro\n" +
-            "3. Eliminar Libro\n" +
-            "4. Volver al Menú Principal\n\n" +
-            "Seleccione una opción:"
-        );
+    public static void prestamosDevoluciones() {
+        int opcion;
 
-        if (seleccion.equals("1")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Agregar Libro");
-            mostrarMenuAdministrarLibros();
-        } else if (seleccion.equals("2")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Modificar Libro");
-            mostrarMenuAdministrarLibros();
-        } else if (seleccion.equals("3")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Eliminar Libro");
-            mostrarMenuAdministrarLibros();
-        } else if (seleccion.equals("4")) {
-            mostrarMenuPrincipal();
-        } else {
-            JOptionPane.showMessageDialog(null, "Opción inválida.");
-            mostrarMenuAdministrarLibros();
-        }
+        do {
+            String seleccion = JOptionPane.showInputDialog(
+                " PRÉSTAMOS Y DEVOLUCIONES\n" +
+                "1. Realizar Préstamo\n" +
+                "2. Realizar Devolución\n" +
+                "3. Volver al Menú Principal\n\n" +
+                "Seleccione una opción:"
+            );
+
+            if (seleccion == null) break;
+
+            opcion = Integer.parseInt(seleccion);
+
+            switch (opcion) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Realizar Préstamo");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Realizar Devolución");
+                    break;
+                case 3:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+
+        } while (opcion != 3);
     }
 
-    public static void mostrarMenuPrestamosDevoluciones() {
-        String seleccion = JOptionPane.showInputDialog(
-            " PRÉSTAMOS Y DEVOLUCIONES\n" +
-            "1. Realizar Préstamo\n" +
-            "2. Realizar Devolución\n" +
-            "3. Volver al Menú Principal\n\n" +
-            "Seleccione una opción:"
-        );
+    public static void consultarLibros() {
+        int opcion;
 
-        if (seleccion.equals("1")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Realizar Préstamo");
-            mostrarMenuPrestamosDevoluciones();
-        } else if (seleccion.equals("2")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Realizar Devolución");
-            mostrarMenuPrestamosDevoluciones();
-        } else if (seleccion.equals("3")) {
-            mostrarMenuPrincipal();
-        } else {
-            JOptionPane.showMessageDialog(null, "Opción inválida.");
-            mostrarMenuPrestamosDevoluciones();
-        }
+        do {
+            String seleccion = JOptionPane.showInputDialog(
+                " CONSULTAR LIBROS\n" +
+                "1. Consultar por Título\n" +
+                "2. Consultar por Autor\n" +
+                "3. Mostrar Todos los Libros\n" +
+                "4. Volver al Menú Principal\n\n" +
+                "Seleccione una opción:"
+            );
+
+            if (seleccion == null) break;
+
+            opcion = Integer.parseInt(seleccion);
+
+            switch (opcion) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Consultar por Título");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Consultar por Autor");
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Funcionalidad: Mostrar Todos los Libros");
+                    break;
+                case 4:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+
+        } while (opcion != 4);
     }
-
-    public static void mostrarMenuConsultar() {
-        String seleccion = JOptionPane.showInputDialog(
-            " CONSULTAR LIBROS\n" +
-            "1. Consultar por Título\n" +
-            "2. Consultar por Autor\n" +
-            "3. Mostrar Todos los Libros\n" +
-            "4. Volver al Menú Principal\n\n" +
-            "Seleccione una opción:"
-        );
-        
-        if (seleccion.equals("1")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Consultar por Título");
-            mostrarMenuConsultar();
-        } else if (seleccion.equals("2")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Consultar por Autor");
-            mostrarMenuConsultar();
-        } else if (seleccion.equals("3")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad: Mostrar Todos los Libros");
-            mostrarMenuConsultar();
-        } else if (seleccion.equals("4")) {
-            mostrarMenuPrincipal();
-        } else {
-            JOptionPane.showMessageDialog(null, "Opción inválida.");
-            mostrarMenuConsultar();
-        }
-    }
-    
-
 }
+    
+       
         
       
        
